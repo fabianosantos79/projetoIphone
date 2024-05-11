3ee4cb6af52cc558ab49827fe3c496d7ac5724da
Fabiano <fabiano.ssantos79@gmail.com>
2024-05-11 10:08:12 -0300

    init project

---
+++ src/main/java/Aplicacao.java
@@ -0,0 +1,14 @@
+public class Aplicacao {
+    public static void main(String[] args) {
+        Iphone iphone1 = new Iphone();
+        iphone1.ligar();
+        iphone1.atender();
+        iphone1.iniciarCorreioVoz();
+        iphone1.adicionarNovaAba();
+        iphone1.exibirPagina();
+        iphone1.atualizarPagina();
+        iphone1.selecionarMusica();
+        iphone1.tocar();
+        iphone1.pausar();
+    }
+}
---
+++ src/main/java/Iphone.java
@@ -0,0 +1,47 @@
+public class Iphone implements ReprodutorMusical, AparelhoTelefonico, NavegadorInternet {
+
+    @Override
+    public void ligar() {
+        System.out.println("Ligando...");
+    }
+
+    @Override
+    public void atender() {
+        System.out.println("Atendendo...");
+    }
+
+    @Override
+    public void iniciarCorreioVoz() {
+        System.out.println("Abrir correio de voz...");
+    }
+
+    @Override
+    public void exibirPagina() {
+        System.out.println("Exibir página...");
+    }
+
+    @Override
+    public void adicionarNovaAba() {
+        System.out.println("Abrir nova aba...");
+    }
+
+    @Override
+    public void atualizarPagina() {
+        System.out.println("Atualizar página...");
+    }
+
+    @Override
+    public void tocar() {
+        System.out.println("Tocar música...");
+    }
+
+    @Override
+    public void pausar() {
+        System.out.println("Pausar música...");
+    }
+
+    @Override
+    public void selecionarMusica() {
+        System.out.println("Selecionar música...");
+    }
+}
